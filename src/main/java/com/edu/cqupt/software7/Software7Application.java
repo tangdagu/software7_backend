5d0270c654b1656a8e3651a49b0f945a015ff74c
zongqingyang <zongqingyang@hotmail.com>
2023-09-03 16:12:52 +0800

    Initial commit

---
+++ src/main/java/com/edu/cqupt/software7/Software7Application.java
@@ -0,0 +1,13 @@
+package com.edu.cqupt.software7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Software7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Software7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cqupt/software7/Software7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cqupt.software7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Software7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
